f3843e9cb50d1c023bf976cdf2bb592b6f199029
Erlangga Yudi Pradana <erlanggayudipradana@gmail.com>
2024-06-10 16:45:10 +0700

    chore: add define guards to utils and update CMakeLists

--- src/utils.h
+++ src/utils.h
@@ -1,4 +1,9 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
 #include "inference.h"
 
 void DrawDetectedObject(cv::Mat &frame, const std::vector<yolo::Detection> &detections, const std::vector<std::string> &class_names);
 std::vector<std::string> GetClassNameFromMetadata(const std::string &metadata_path);
+
+#endif // UTILS_H_
